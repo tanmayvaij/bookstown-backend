63be47a871fdb7e61b81baf7de1395cb2ed73f21
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-10-23 17:39:48 +0530

    project restarted

--- server/db/index.js
+++
@@ -1,8 +0,0 @@
-const { connect } = require('mongoose')
-
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
-
-connect( process.env.MONGO_URI, options, (err) => {
-    if (err) console.log(err)
-    else console.log('database connected successfully')
-} )
