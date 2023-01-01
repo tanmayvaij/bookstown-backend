ff4815b598540b7d3824331b3368f013d44dfe9c
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-01-01 19:21:46 +0530

    completed user sign in

--- src/routes/SellerAuth/controller.ts
+++ src/routes/SellerAuth/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { sign } from "jsonwebtoken"
 import { SellerSchema } from "./schema"
+import { compare } from "bcrypt"
 
 
 // handler for seller signup 
@@ -19,15 +20,20 @@ export const handleSignUp = async (req: Request, res: Response) => {
 
     try {
 
+        // Saving details to database
         const seller = await SellerSchema.create({ name, email, number, address, password })
 
+        // getting doc id
         const id = seller.toObject()._id.toString()
 
+        // creating payload object
         const payload = { id, name, email, number, address }
 
+        // creating token
         const authtoken = sign(payload, process.env.JWT_SECRET as string)
 
-        res.json(authtoken)
+        // sending token
+        res.json({ success:true, authtoken })
 
     }
 
@@ -36,8 +42,33 @@ export const handleSignUp = async (req: Request, res: Response) => {
 }
 
 
-export const handleSignIn = (req: Request, res: Response) => {
+// handler for seller signin
+export const handleSignIn = async (req: Request, res: Response) => {
 
-    
+    // getting email, password from user
+    const { email, password } = req.body
+
+    // checking if fields are empty or not. Returning error message if fields are missing
+    if ( email == "" || password == "" ) return res.json({ success: false, message: "Missing fields" })
+
+    // checking if user exists or not, if not then showing error message
+    const seller = await SellerSchema.findOne({ email })
+    if (!seller) return res.json({ success: false, message: "Invalid Credentials" })
+
+    // checking if password is correct or not. Returning error message if password is wrong
+    const match = await compare(password, seller.password)
+    if (!match) return res.json({ success: false, message: "Invalid Credentials" })
+
+    // getting seller details
+    const { _id, name, number, address } = seller
+
+    // creating payload
+    const payload = { _id, name, email, number, address }
+
+    // generating token
+    const authtoken = sign(payload, process.env.JWT_SECRET as string)
+
+    // sending token
+    res.json({ success: true, authtoken })
 
 }
